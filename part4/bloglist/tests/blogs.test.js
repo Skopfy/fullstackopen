75febb81c373e85fcbcd2bc1cfe79109a3d8576c
Skopfy <christian.roth.1995.de@gmail.com>
2023-01-06 13:24:43 +0100

    Finished 4.5

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -1,25 +1,6 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
-})
-
-describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const multiple_blogs = [
+const multiple_blogs = [
   {
     _id: "5a422a851b54a676234d17f7",
     title: "React patterns",
@@ -70,6 +51,28 @@ describe('total likes', () => {
   }  
 ]
 
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+    
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  
+
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     expect(result).toBe(5)
@@ -78,5 +81,45 @@ describe('total likes', () => {
   test('when list has multiple blogs, equals the likes of all of those added up', () => {
     const result = listHelper.totalLikes(multiple_blogs)
     expect(result).toBe(36)
+  })
+
+test('when list is empty, return zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+})
+
+describe('favorite blog', () => {
+    test('when list is empty, return empty Object', () => {
+    const result = listHelper.favoriteBlog([])
+	expect(result).toEqual({})
+    })
+
+    test('when list has one element, return that one', () => {
+    const result = listHelper.favoriteBlog([multiple_blogs[0]])
+	expect(result).toEqual(
+	    {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  }
+	)
+    })
+
+    test('in list with multiple blogs return the one with highest no of likes', () => {
+    const result = listHelper.favoriteBlog(multiple_blogs)
+	expect(result).toEqual(
+	    {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+	)
   })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,8 +10,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return {}
+    var favorite = blogs[0]
+    for (const blog of blogs){
+	if (blog.likes > favorite.likes){
+	    favorite = blog
+	}
+    }
+  return favorite
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
     
 }

a92499c0bf8322f310c0ed0d9fedc7f8e9dbd831
Skopfy <christian.roth.1995.de@gmail.com>
2023-04-15 15:05:10 +0200

    5.18 done

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -11,4 +11,22 @@ describe('Blog app', function () {
     cy.get('#username').type('user')
     cy.get('#password').type('password')
   })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('user')
+      cy.get('#password').type('password')
+      cy.get('#login-button').click()
+      cy.contains('user logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('usa')
+      cy.get('#password').type('passwort')
+      cy.get('#login-button').click()
+      cy.get('.error').should('contain', 'Wrong credentials')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.get('.error').should('have.css', 'border-style', 'solid')
+    })
+  })
 })

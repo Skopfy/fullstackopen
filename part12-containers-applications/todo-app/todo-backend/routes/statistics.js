6473a6aaf50481fc6c37eec631f4566f3a93e92c
Skopfy <christian.roth.1995.de@gmail.com>
2023-04-25 12:19:51 +0200

    12.12 done

--- part12-containers-applications/todo-app/todo-backend/routes/statistics.js
+++ part12-containers-applications/todo-app/todo-backend/routes/statistics.js
@@ -5,6 +5,9 @@ const redis = require('../redis')
 /* GET added_todos. */
 router.get('/', async (_, res) => {
     const added_todos = await redis.getAsync("added_todos")
+    if (added_todos === undefined) {
+        await redis.setAsync("added_todos", 0)
+    }
     res.send({"added_todos": added_todos});
 });
 
